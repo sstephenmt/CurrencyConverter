1c9a02af31846de0191bb8363b1820bd5867d07b
sstephenmt <72407248@continental.edu.pe>
2020-11-12 07:44:06 -0500

    Agregar archivos de proyecto.

---
+++ CurrencyConverterForns/FrmCurrency.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverterForms
+{
+    public partial class FrmCurrency : Form
+    {
+        public FrmCurrency()
+        {
+            InitializeComponent();
+        }
+
+        private decimal AusInUS = 2;
+        private decimal UKInUS = 0.5M;
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            decimal converted = 0.0M;
+            decimal initial = 0.0M;
+
+            initial = Convert.ToDecimal(Amount.Text);
+            converted = initial;
+
+            if (FromUK.Checked)
+            {
+                converted = initial / UKInUS;
+            }
+            else if (FromAUS.Checked)
+            {
+                converted = initial / AusInUS;
+            }
+
+            if (ToUK.Checked)
+            {
+                converted = converted * UKInUS;
+            }
+            else if (ToAus.Checked)
+            {
+                converted = converted * AusInUS;
+            }
+
+            Result.Text = converted.ToString();
+        }
+    }
+}
